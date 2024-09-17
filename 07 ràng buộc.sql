60454cbfe5fa8fd24e52bf12f6655ff5ccfdeb26
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-17 23:36:38 +0700

    Update 07 ràng buộc.sql

--- 07 ràng buộc.sql
+++ 07 ràng buộc.sql
@@ -1,8 +1,12 @@
-CREATE TABLE database.my_table (
-  id int NOT NULL; -- đảm bảo không nhận giá trị null
-  id2 int CHECK;  -- đảm bảo giá trị trong cột thỏa mãn tính chất cụ thể
-  id3 int DEFAULT;  -- trả về giá trị default nếu trong cột không có giá trị chỉ định
-)
--- thay đổi ràng buộc bằng cách:
+CREATE TABLE levinh.my_table (
+  id int NOT NULL, -- đảm bảo không nhận giá trị null
+  id2 int CHECK,  -- đảm bảo giá trị trong cột thỏa mãn tính chất cụ thể
+  id3 int DEFAULT 0  -- trả về giá trị default = 0 nếu trong cột không có giá trị chỉ định
+);
+-- thêm ràng buộc bằng 2 cách:
 ALTER TABLE my_table
-MODIFY id   int PRIMARY KEY;  -- chuyển id từ NOT NULL thành khóa chính
+MODIFY id   varchar(255) PRIMARY KEY;  -- id từ int thêm ràng buộc primary key
+-- hoặc:
+ALTER TABLE my_table
+ADD UNIQUE (id); -- cách này thì không sửa được kiểu dữ liệu của id  
+
