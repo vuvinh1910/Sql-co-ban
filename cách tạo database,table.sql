e64563d0db7cd9af6fc2bf92c31164a3784a1b8b
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-17 21:23:05 +0700

    Update and rename cách tạo database.sql to cách tạo database,table.sql

--- cách tạo database,table.sql
+++ cách tạo database,table.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE yourdatabase_name; -- tạo database mới
 DROP DATABASE yourdatabase_name;  -- xóa database
 -- trong database ta tạo bảng table bằng cách:
--- chọn chuột 2 lần vào database muốn tạo thêm bảng ở trong nó rồi nhập lệnh:
-CREATE TABLE yourtable_name (
+
+CREATE TABLE yourdatabase_name.yourtable_name (  -- dấu chấm ở đây nghĩa là tạo bảng có vị trí trong yourdatabase_name 
   tên_cột_1    kiểu_dữ_liệu_cột_1,
   tên_cột_2    kiểu_dữ_liệu_cột_2,
   tên_cột_3    kiểu_dữ_liệu_cột_3,
   .....
 );
 -- ví dụ:
+-- đây là bảng trống không có dữ liệu. 
 CREATE TABLE NhanVien (
   ID      int,
-  Name    varchar(255),
+  FullName    varchar(255),  
   Address    varchar(255)  -- cuối không còn dấu , 
 );
