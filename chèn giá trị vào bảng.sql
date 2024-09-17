b006c24a85b78c68d08e85bc96bcda92f1847f7f
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-17 21:32:52 +0700

    Create chèn giá trị vào bảng.sql

---
+++ chèn giá trị vào bảng.sql
@@ -0,0 +1,14 @@
+-- cú pháp:
+INSERT INTO ten_bang (cot1, cot2, cot3, ...)
+VALUES (giatri1, giatri2, giatri3, ...);
+
+-- chèn 1 hàng:  
+INSERT INTO sinhvien (id, ho, ten, tuoi)
+VALUES (1, 'Nguyen', 'An', 20);
+
+-- chèn nhiều hàng:
+INSERT INTO sinhvien (id, ho, ten, tuoi)
+VALUES
+(2, 'Le', 'Hoa', 22),
+(3, 'Pham', 'Minh', 21),
+(4, 'Tran', 'Lam', 19);
