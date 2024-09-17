455ccd04cee03ca2ee5eabf9c5d18cf1faf63ea5
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-17 22:50:00 +0700

    Update 05 khóa ngoại và liên kết bảng qua khóa.sql

--- 05 khóa ngoại và liên kết bảng qua khóa.sql
+++ 05 khóa ngoại và liên kết bảng qua khóa.sql
@@ -14,3 +14,18 @@ CREATE TABLE LopHoc (
     ON DELETE CASCADE              -- Xóa sinh viên sẽ xóa luôn bản ghi tương ứng trong LopHoc
     ON UPDATE CASCADE              -- Khi cập nhật id trong SinhVien, LopHoc cũng cập nhật theo
 );
+
+-- quan trọng đây là tham chiếu, nên giá trị khóa chính và khóa ngoại muốn liên kết phải giống nhau
+-- vd: 
+INSERT INTO SinhVien (id, ho, ten, tuoi) 
+VALUES 	(1, 'Nguyen', 'Van A1', 20),
+		(2, 'Nguyen', 'Van A2', 20),
+        (3, 'Nguyen', 'Van A3', 20),
+        (4, 'Nguyen', 'Van A4', 20)
+;
+INSERT INTO LopHoc (id, ten_lop,id_sinhvien) 
+VALUES 	(101, 'Lop Toan 1',3), -- bạn A3 học lớp toán 1
+		(102, 'Lop Toan 2',4), -- bạn A4 học lớp toán 2
+        (103, 'Lop Toan 3',1), -- bạn A1 học lớp toán 3
+        (104, 'Lop Toan 4',2); -- bạn A2 học lớp toán 4
+
