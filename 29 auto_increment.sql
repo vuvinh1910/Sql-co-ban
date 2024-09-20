669a18d77cfd37f1c0979f819ed4ce437160ccfc
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-20 21:59:10 +0700

    Create 29 auto_increment.sql

---
+++ 29 auto_increment.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tac_gia(
+	id int auto_increment primary key not null,  -- id sẽ tự động tăng khi đc thêm vào hàng.
+    ten nvarchar(255) not null,
+    but_danh nvarchar(255) not null
+);
