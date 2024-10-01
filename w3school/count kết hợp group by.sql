b547c5c46e30b924b094a7612e070c1a1b981395
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-10-01 23:02:22 +0700

    Update count kết hợp group by.sql

--- w3school/count kết hợp group by.sql
+++ w3school/count kết hợp group by.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(*) AS [Number of records], CategoryID
+SELECT COUNT(*) AS Number_records, CategoryID
 FROM Products
 GROUP BY CategoryID;
 -- trả về số Hồ sơ cho từng danh mục trong bảng Sản phẩm
