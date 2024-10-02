b726d22eebbbe0295cdb3e30d43f6f1d09eb8d28
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-10-02 12:02:38 +0700

    Update 24 UNION.sql

--- 24 UNION.sql
+++ 24 UNION.sql
@@ -8,12 +8,18 @@ FROM table2;
 -- giống như add trong set, các dữ liệu trùng nhau thì không thêm, các dữ liệu khác nhau thì thêm.
 -- vd: 1,2,3 union 2,3,4 thì = 1,2,3,4.
 
+
 -- đây là set họ tên của 2 bảng dai hoc và cao đẳng.
-SELECT ho, ten
-FROM SinhVien_DaiHoc
+SELECT City,Address FROM Customers
 UNION
-SELECT ho, ten
-FROM SinhVien_CaoDang;
+SELECT City,Address FROM Suppliers
+ORDER BY City;
+-- nếu bạn thấy kết quả trong cột city hoặc address có trùng lặp
+-- thì thực chất nó là khác nhau khi xét về việc nối city và address ví dụ:
+-- london - hanoi
+-- khác với london - nghean
+-- ta xét theo việc nối 2 kết quả nới union
+
 
 
 -- nếu không muốn loại bỏ giá trị trùng lặp thì ta dùng UNION ALL:
